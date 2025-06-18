76df9f0df128e513d84d96ab2d3860182a0caa03
bluemagikSGO <stevengabriel2015@gmail.com>
2025-06-18 20:53:04 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import bitcoin from "../assets/bitcoin.png"; // Replace with your own image
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-between lg:ml-15">
+        {/* Left: Text Content */}
+        <div className="lg:w-1/2 text-center lg:text-left lg:ml-16">
+          <h1 className="text-4xl sm:text-7xl font-bold text-gray-900 leading-tight mb-6">
+            Your gateway to <br className="hidden sm:block" />
+            <span className="text-blue-600">smart crypto investing.</span>
+          </h1>
+          <p className="text-xl text-gray-600 mb-6">
+            Invest in the world's digital future securely.
+          </p>
+          <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-lg shadow transition-all">
+            Get Started
+          </button>
+        </div>
+
+        {/* Right: Image */}
+        <div className="lg:w-1/2 mb-12 lg:mb-0  flex justify-center">
+          <img
+            src={bitcoin}
+            alt="Bitcoin phone"
+            className="w-[300px] sm:w-[350px] md:w-[450px] lg:mt-10"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+import { Menu, X } from "lucide-react";
+
+const NavBar = () => {
+  const [Menuon, setMenuon] = useState(false);
+
+  const togglemenu = () => {
+    setMenuon(!Menuon);
+  };
+  return (
+    <div>
+      <div className="flex justify-between p-7 items-center text-xl font-semibold shadow-lg">
+        <img src={logo} alt="logo" />
+        <ul className="hidden md:flex space-x-10 items-center">
+          <li className="hover:text-blue-500">About Us</li>
+          <li className="hover:text-blue-500">Staff card</li>
+          <li>
+            <button className="ring-1 p-3 rounded-xl bg-blue-500 text-white hover:bg-white hover:ring-1 hover:text-black cursor-pointer">
+              <a href="https://my.novabeak.com/">Click to Invest</a>
+            </button>
+          </li>
+        </ul>
+        <div onClick={togglemenu} className="md:hidden">
+          {Menuon ? <X size={45} /> : <Menu size={45} />}
+        </div>
+      </div>
+      {/* mobile menu */}
+      {Menuon && (
+        <ul className="md:hidden absolute bg-[#f5faff] w-full flex flex-col text-3xl font-semibold gap-3 pl-7 border-b-1 pb-5 mt-7">
+          <li className="hover:text-blue-500">About Us</li>
+          <li className="hover:text-blue-500">Staff card</li>
+          <li>
+            <button className="ring-1 p-3 rounded-xl bg-blue-500 text-white hover:bg-white hover:ring-1 hover:text-black cursor-pointer">
+              <a href="https://my.novabeak.com/">Click to Invest</a>
+            </button>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
